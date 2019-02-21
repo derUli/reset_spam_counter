cd5778d47e07ca84f758da134810568fe235da86
Ulrich Schmidt <admin@deruli.de>
2019-02-21 23:01:29 +0100

    reset_spam_counter

--- src/content/modules/reset_spam_counter/templates/settings.php
+++ src/content/modules/reset_spam_counter/templates/settings.php
@@ -1,3 +1,4 @@
 <?php echo ModuleHelper::buildMethodCallForm("ResetSpamCounter", "doReset");?>
-<input type="submit" value="<?php translate("reset_spam_counter");?>">
+<button type="submit" name="submit" class="btn btn-danger"><i class="fa fa-sync" aria-hidden="true"></i> 
+<?php translate("reset_spam_counter");?></button>
 </form>
